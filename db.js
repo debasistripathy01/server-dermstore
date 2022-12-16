b65073c53c4c04ba7bbd91984be713f7b68e4c2b
debasistripathy01 <debasistripathy04@gmail.com>
2022-12-16 16:56:20 +0530

    update

---
+++ db.js
@@ -0,0 +1,29 @@
+// const items = require("./data/items");
+// const tags = require("./data/tags");
+// const brands = require("./data/brands");
+// const itemTypes = require("./data/itemTypes");
+// const jiomarthomedata = require("./data/jiomarthomedata.json");
+// const react = require("./React/react.json");
+// const dermcart = require("./DermStore/dermcart.json");
+// const dermproducts = require("./DermStore/dermproducts.json");
+// const dermuserinfo = require("./DermStore/dermuserinfo.json");
+// const bookdetails = require("./React/abhishek_bookdetails.json");
+// const signupab = require("./React/abhishek_signup.json");
+const products = require("./DataDermStore/products.json")
+// module.exports = {
+//   items,
+//   tags,
+//   brands,
+//   itemTypes,
+//   jiomarthomedata,
+//   react,
+//   dermcart,
+//   dermproducts,
+//   dermuserinfo,
+//   bookdetails,
+//   signupab,
+// };
+
+module.exports={
+    products
+}
