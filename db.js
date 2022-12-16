aebe80ade76a3f094f5e1f77eeedfe2fa24b1d53
debasistripathy01 <debasistripathy04@gmail.com>
2022-12-16 17:29:25 +0530

    updated

--- db.js
+++ db.js
@@ -11,6 +11,18 @@
 // const signupab = require("./React/abhishek_signup.json");
 const men = require("./DataDermStore/men.json")
 const products = require("./DataDermStore/products.json")
+const bestsellers = require("./DataDermStore/BestSellers.json")
+const Blog = require("./DataDermStore/Blog.json")
+const cart = require("./DataDermStore/cart.json")
+const cartproduct = require("./DataDermStore/CartProduct.json")
+const extradata = require("./DataDermStore/extraData.json")
+const hair = require("./DataDermStore/hair.json")
+const neocutis = require("./DataDermStore/Neocutis.json")
+const newarrivals = require("./DataDermStore/NewArrivals.json")
+const productsdata = require("./DataDermStore/productsData.json")
+const skincare = require("./DataDermStore/SkinCare.json")
+const trending = require("./DataDermStore/Trending.json")
+
 // module.exports = {
 //   items,
 //   tags,
@@ -26,6 +38,10 @@ const products = require("./DataDermStore/products.json")
 // };
 
 module.exports={
+    bestsellers,
+    Blog,
+    cart,cartproduct, extradata, hair,neocutis, 
+    newarrivals, productsdata, skincare, trending, 
     products,
     men
 }
