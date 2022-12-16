ba8acb113ae7d7aaedfe4bf7bfbb34d502d5180a
debasistripathy01 <debasistripathy04@gmail.com>
2022-12-16 21:59:10 +0530

    server

--- db.js
+++ db.js
@@ -1,14 +1,4 @@
-// const items = require("./data/items");
-// const tags = require("./data/tags");
-// const brands = require("./data/brands");
-// const itemTypes = require("./data/itemTypes");
-// const jiomarthomedata = require("./data/jiomarthomedata.json");
-// const react = require("./React/react.json");
-// const dermcart = require("./DermStore/dermcart.json");
-// const dermproducts = require("./DermStore/dermproducts.json");
-// const dermuserinfo = require("./DermStore/dermuserinfo.json");
-// const bookdetails = require("./React/abhishek_bookdetails.json");
-// const signupab = require("./React/abhishek_signup.json");
+
 const men = require("./DataDermStore/men.json")
 const products = require("./DataDermStore/products.json")
 const bestsellers = require("./DataDermStore/BestSellers.json")
@@ -23,19 +13,7 @@ const productsdata = require("./DataDermStore/productsData.json")
 const skincare = require("./DataDermStore/SkinCare.json")
 const trending = require("./DataDermStore/Trending.json")
 
-// module.exports = {
-//   items,
-//   tags,
-//   brands,
-//   itemTypes,
-//   jiomarthomedata,
-//   react,
-//   dermcart,
-//   dermproducts,
-//   dermuserinfo,
-//   bookdetails,
-//   signupab,
-// };
+
 
 module.exports={
     bestsellers,
